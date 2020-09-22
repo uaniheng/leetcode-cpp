12ed187caf47f250184b75cacc1d79bc90e32e91
uaniheng <uaniheng@qq.com>
2020-09-22 17:19:28 +0800

    752 双向BFS

--- 202009/21/_752_OpentheLock.cpp
+++ 202009/21/_752_OpentheLock.cpp
@@ -2,69 +2,69 @@
 // Created by gyc on 2020/9/21.
 //
 #include "../../common.h"
-#include <unordered_set>
-
-
 
 class Solution {
 private:
-
-
     string upState(string state, int index) {
-        if (state[index] == '9'){
+        if (state[index] == '9') {
             state[index] = '0';
         } else {
-            state[index] ++;
+            state[index]++;
         }
         return state;
     }
 
     string downState(string state, int index) {
 
-        if (state[index] == '0'){
+        if (state[index] == '0') {
             state[index] = '9';
-        }else {
-            state[index] --;
+        } else {
+            state[index]--;
         }
         return state;
     }
-public:
-    int openLock(vector<string>& deadends, string target) {
-        set<string> visited;
 
+public:
+    int openLock(vector<string> &deadends, string target) {
+        set<string> visited;
         visited.insert(deadends.begin(), deadends.end());
         if (visited.count("0000")) return -1;
 
         int num = 0;
+        auto *q1 = new unordered_set<string>();
+        auto *q2 = new unordered_set<string>();
 
-        queue<string> que{};
-        que.push("0000");
+        q1->emplace("0000");
+        q2->emplace(target);
+
         visited.insert("0000");
 
-        while (!que.empty()) {
-            int size = que.size();
-            for (int i = 0; i < size; i ++) {
-                string state = que.front();
-                que.pop();
+        while (!q1->empty() && !q2->empty()) {
 
-                if (state == target) {
+            auto *temp = new unordered_set<string>();
+
+            for (const string &curr: *q1) {
+
+                if (q2->count(curr)) {
                     return num;
                 }
-
-                for(int j = 0; j < 4; j ++) {
-                    string up = upState(state, j);
-                    string down = downState(state, j);
+                visited.emplace(curr);
+                for (int j = 0; j < 4; j++) {
+                    string up = upState(curr, j);
+                    string down = downState(curr, j);
                     if (!visited.count(up)) {
-                        visited.insert(up);
-                        que.push(up);
+                        //visited.emplace(up);
+                        temp->emplace(up);
                     }
                     if (!visited.count(down)) {
-                        visited.insert(down);
-                        que.push(down);
+                        //visited.emplace(down);
+                        temp->emplace(down);
                     }
                 }
             }
-            num ++;
+            num++;
+            q1 = q2;
+            q2 = temp;
         }
         return -1;
     }
@@ -73,8 +73,6 @@ public:
 int main() {
     // ["0201","0101","0102","1212","2002"]
     //"0202"
-
-    vector<string> deadends{"0201","0101","0102","1212","2002"};
-
+    vector<string> deadends{};
     cout << Solution().openLock(deadends, "0202") << endl;
 }
--- common.h
+++ common.h
@@ -11,6 +11,7 @@
 #include <queue>
 #include <stack>
 #include <set>
+#include <unordered_set>
 
 using namespace std;
 
