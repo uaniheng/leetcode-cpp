731ae3bbd410e98e6ec112fe1bfb772116af60fb
uaniheng <uaniheng@qq.com>
2020-09-28 14:15:25 +0800

    123 股票问题 空间优化

--- 202009/28/_123_BestTimeToBuyAndSellStock3.cpp
+++ 202009/28/_123_BestTimeToBuyAndSellStock3.cpp
@@ -10,20 +10,14 @@ public:
         if (prices.empty()) return 0;
         int n = prices.size();
 
-        int dp[n][3][2];
-        dp[0][0][1] = -prices[0];
-        dp[0][1][0] = 0;
-        dp[0][1][1] = -prices[0];
-        dp[0][2][0] = 0;
-        dp[0][2][1] = -prices[0];
-
-        for (int i = 1; i < n; ++i) {
-            dp[i][2][0] = max(dp[i - 1][2][0], dp[i - 1][2][1] + prices[i]);
-            dp[i][2][1] = max(dp[i - 1][2][1], dp[i - 1][1][0] - prices[i]);
-            dp[i][1][0] = max(dp[i - 1][1][0], dp[i - 1][1][1] + prices[i]);
-            dp[i][1][1] = max(dp[i - 1][1][1], - prices[i]);
+        int dp10 = 0, dp11 = -prices[0], dp20 = 0, dp21 = -prices[0];
+        for (int i = 0; i < n; ++i) {
+            dp10 = max(dp10, dp11 + prices[i]);
+            dp11 = max(dp11, -prices[i]);
+            dp20 = max(dp20, dp21 + prices[i]);
+            dp21 = max(dp21, dp10 - prices[i]);
         }
-        return dp[n - 1][2][0];
+        return dp20;
     }
 };
 
