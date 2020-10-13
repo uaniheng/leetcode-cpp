887e7ec6e7a4adf7edb767f201052b97cd754f0e
uaniheng <uaniheng@qq.com>
2020-10-13 16:51:59 +0800

    72 编辑距离 递归+备忘录

--- 202009/30/_72_EditDistance.cpp
+++ 202009/30/_72_EditDistance.cpp
@@ -5,34 +5,35 @@
 
 class Solution {
 
-private:
-private:
-    unordered_map<string, int> note;
-
-
-    int dp(int i, int j, string &w1, string &w2) {
-        string key = to_string(i) + ":" + to_string(j);
-        if (note.count(key)) return note[key];
-        if (i == -1 || j == -1) return max(i, j) + 1;
-
-        if (w1[i] == w2[j]) {
-            return dp(i - 1, j - 1, w1, w2);
-        }
-        int res = min({dp(i - 1, j - 1, w1, w2) + 1,
-                       dp(i, j - 1, w1, w2) + 1,
-                       dp(i - 1, j, w1, w2) + 1});
-        note[key] = res;
-        return res;
-    }
-
 public:
     int minDistance(string word1, string word2) {
 
-        return dp(word1.size() - 1, word2.size() - 1, word1, word2);
+        int m = word1.size(), n = word2.size();
+        if (m == 0 || n == 0) return n + m;
+
+        int dp[m + 1][n + 1];
+
+        for(int i = 0; i <= m; ++i) {
+            dp[i][0] = i;
+        }
+        for(int i = 0; i <= n; ++i) {
+            dp[0][i] = i;
+        }
+
+        for(int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (word1[i - 1] == word2[i - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } {
+                    dp[i][j] = min({dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]}) + 1;
+                }
+            }
+        }
+        return dp[word1.size()][word2.size()];
     }
 };
 
 int main() {
 
-    cout << Solution().minDistance("horse", "ros");
+    cout << Solution().minDistance("apple", "rad");
 }
