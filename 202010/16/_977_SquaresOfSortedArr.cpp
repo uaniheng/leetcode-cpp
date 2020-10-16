7a420d495f3506dcca79cc2b718d2243a3ac3f95
uaniheng <uaniheng@qq.com>
2020-10-16 11:27:58 +0800

    977 双指针 由两边向中间归并 无需判断边界

--- 202010/16/_977_SquaresOfSortedArr.cpp
+++ 202010/16/_977_SquaresOfSortedArr.cpp
@@ -5,46 +5,32 @@
 
 class Solution {
 public:
-    vector<int> sortedSquares(vector<int>& A) {
+    vector<int> sortedSquares(vector<int> &A) {
 
-        int neg = -1;
-        for(int i = 0; i < A.size(); ++i) {
-            if (A[i] < 0) {
-                neg = i;
+
+        int n = A.size();
+        vector<int> res(n);
+        for (int i = 0, j = n - 1, pos = n - 1; i <= j;) {
+
+            if (A[i] * A[i] > A[j] * A[j]) {
+                res[pos] = A[i] * A[i];
+                ++i;
+            } else {
+                res[pos] = A[j] * A[j];
+                --j;
             }
-        }
-
-        vector<int> res{};
-
-        int pos = neg + 1;
-
-        while (neg > -1 || pos < A.size()) {
-
-            if (neg < 0) {
-                res.push_back(A[pos] * A[pos]);
-                pos ++;
-            }else if (pos == A.size()) {
-                res.push_back(A[neg] * A[neg]);
-                neg --;
-            }else if (A[neg] * A[neg] > A[pos] * A[pos]) {
-                res.push_back(A[pos] * A[pos]);
-                pos ++;
-            }else {
-                res.push_back(A[neg] * A[neg]);
-                neg --;
-            }
+            --pos;
         }
         return res;
     }
 };
 
 int main() {
-    vector<int> vec{-4,-1,0,3,10};
+    vector<int> vec{-4, -1, 0, 3, 10};
 
     auto res = Solution().sortedSquares(vec);
 
-    for(int i: res) {
+    for (int i: res) {
         cout << i << endl;
     }
-
 }
