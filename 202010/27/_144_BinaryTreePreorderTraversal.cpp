1864c26d13b7f16189f0d00cbb6b8090cb86412b
uaniheng <uaniheng@qq.com>
2020-10-27 16:41:07 +0800

    144 二叉树遍历 morris遍历 不懂

--- 202010/27/_144_BinaryTreePreorderTraversal.cpp
+++ 202010/27/_144_BinaryTreePreorderTraversal.cpp
@@ -3,31 +3,50 @@
 //
 #include "../../common.h"
 
+/**
+Morris 遍历的核心思想是利用树的大量空闲指针，实现空间开销的极限缩减。其前序遍历规则总结如下：
+
+1. 新建临时节点，令该节点为 root；
+
+2. 如果当前节点的左子节点为空，将当前节点加入答案，并遍历当前节点的右子节点；
+
+3. 如果当前节点的左子节点不为空，在当前节点的左子树中找到当前节点在中序遍历下的前驱节点：
+
+    - 如果前驱节点的右子节点为空，将前驱节点的右子节点设置为当前节点。然后将当前节点加入答案，并将前驱节点的右子节点更新为当前节点。
+
+    - 如果前驱节点的右子节点为当前节点，将它的右子节点重新设为空。当前节点更新为当前节点的右子节点。
+
+4. 重复步骤 2 和步骤 3，直到遍历结束。
+ */
 class Solution {
-
 public:
-    vector<int> preorderTraversal(TreeNode* root) {
-        vector<int> res{};
-        if (nullptr == root) return res;
+    vector<int> preorderTraversal(TreeNode *root) {
+        vector<int> res;
+        if (root == nullptr) {
+            return res;
+        }
 
-        stack<TreeNode*> ms{};
+        TreeNode *p1 = root, *p2 = nullptr;
 
-        ms.emplace(root);
-
-        while (!ms.empty()) {
-
-            TreeNode *t = ms.top();
-            ms.pop();
-            res.emplace_back(t->val);
-
-            if (t->right != nullptr) {
-                ms.emplace(t->right);
+        while (p1 != nullptr) {
+            p2 = p1->left;
+            if (p2 != nullptr) {
+                while (p2->right != nullptr && p2->right != p1) {
+                    p2 = p2->right;
+                }
+                if (p2->right == nullptr) {
+                    res.emplace_back(p1->val);
+                    p2->right = p1;
+                    p1 = p1->left;
+                    continue;
+                } else {
+                    p2->right = nullptr;
+                }
+            } else {
+                res.emplace_back(p1->val);
             }
-            if (t->left != nullptr) {
-                ms.emplace(t->left);
-            }
+            p1 = p1->right;
         }
         return res;
-
     }
 };
