2ab2f2256ae856f21a2073c1865da8480e68c395
uaniheng <uaniheng@qq.com>
2020-10-29 09:54:52 +0800

    129 二叉树 dfs 逻辑优化

--- 202010/29/_129_SumRoottoLeafNumbers.cpp
+++ 202010/29/_129_SumRoottoLeafNumbers.cpp
@@ -9,19 +9,12 @@ class Solution {
 private:
 
     int dfs(TreeNode *root, int sum) {
-        if (root == nullptr) return sum;
+        if (root == nullptr) return 0;
         sum = sum * 10 + root->val;
         if (root->left == nullptr and root->right == nullptr) {
             return sum;
         }
-        int res = 0;
-        if (root->left != nullptr) {
-            res += dfs(root->left, sum);
-        }
-        if (root->right != nullptr) {
-            res += dfs(root->right, sum);
-        }
-        return res;
+        return dfs(root->left, sum) + dfs(root->right, sum);
     }
 
 
