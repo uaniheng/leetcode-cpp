7a3d9a32fbc33987483373156bc5c146e350dbcb
uaniheng <uaniheng@qq.com>
2020-11-12 17:20:17 +0800

    127 单词接龙 复习 未优化

--- 202011/05/_127_WordLadder.cpp
+++ 202011/05/_127_WordLadder.cpp
@@ -8,48 +8,47 @@ class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string> &wordList) {
         if (beginWord == endWord) return 0;
-
-
         unordered_set<string> unvisited{};
+        unordered_set<string> visited{};
+        for (const auto &s: wordList) { unvisited.emplace(s); }
         queue<string> nodes{};
-        int res = INT32_MAX, d = 0;
-        for (const auto &e: wordList) {
-            unvisited.insert(e);
-        }
         nodes.emplace(beginWord);
 
+        int d = 0;
         while (!nodes.empty()) {
-            ++d;
-            int s = nodes.size();
-            for(int c = 0; c < s; ++c) {
-                string node = nodes.front();
+            int size = nodes.size();
+            for (int i = 0; i < size; ++i) {
+                string s = nodes.front();
                 nodes.pop();
-                if (node == endWord) {
-                    res = min(res, d);
+                visited.emplace(s);
+                if (s == endWord) {
+                    return d + 1;
                 }
-                for (auto it = unvisited.begin(); it != unvisited.end();) {
-                    string str = *it;
+                for(auto it = unvisited.begin(); it != unvisited.end();) {
                     int ne = 0;
-                    for (int i = 0; i < node.size(); ++i) {
-                        if (node[i] != str[i]) {
-                            ne++;
+                    for(int l = 0; l < beginWord.size(); ++l) {
+                        if (s[l] != (*it)[l]){
+                            ++ne;
                         }
                     }
                     if (ne == 1) {
+                        if (!visited.count(*it)) {
+                            nodes.emplace(*it);
+                        }
                         unvisited.erase(it++);
-                        nodes.push(str);
-                    }else{
+                    }else {
                         ++it;
                     }
                 }
             }
+            ++d;
         }
-        return res == INT32_MAX ? 0 : res;
+        return 0;
     }
 };
 
 int main() {
     string beginWord = "a", endWord = "c";
-    vector<string> wordList{"a","b","c"};
+    vector<string> wordList{"a", "b", "c"};
     cout << Solution().ladderLength(beginWord, endWord, wordList) << endl;
 }
