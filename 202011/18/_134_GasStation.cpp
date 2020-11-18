5177bca0393ad466bfac159470c98eb34007accc
uaniheng <uaniheng@qq.com>
2020-11-18 15:43:58 +0800

    134 加油站 优化方法

--- 202011/18/_134_GasStation.cpp
+++ 202011/18/_134_GasStation.cpp
@@ -4,31 +4,28 @@
 
 #include "../../common.h"
 
+/**
+ *
+ * 画图
+ * 1. 最终油量剩余>=0时，才能走全程
+ * 2. 一次遍历，油量最低点的下一站是出发站
+ */
 class Solution {
 
-private:
-    bool canDrive(int start, vector<int>& gas, vector<int>& cost) {
-
-        int len = gas.size();
-        int oil = 0, index = start;
-        do{
-            index = index % len;
-            oil += gas[index];
-            oil -= cost[index];
-            if (oil < 0) {
-                return false;
-            }
-            ++index;
-        }while (index != start);
-        return true;
-    }
-
 public:
-    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        for(int i = 0; i < gas.size(); ++i) {
-            if (canDrive(i, gas, cost)) {
-                return i;
+    int canCompleteCircuit(vector<int> &gas, vector<int> &cost) {
+
+        int curr = gas[0] - cost[0];
+        vector<int> min{0, gas[0] - cost[0]};
+        for (int i = 1; i < gas.size(); ++i) {
+            curr += gas[i] - cost[i];
+            if (curr < min[1]) {
+                min[0] = i;
+                min[1] = curr;
             }
+        }
+        if (curr >= 0) {
+            return (min[0] + 1) % gas.size();
         }
         return -1;
     }
@@ -36,7 +33,8 @@ public:
 
 int main() {
 
-    vector<int> gas{1,2,3,4,5}, cost{3,4,5,1,2};
+
+    vector<int> gas{3,1,1}, cost{1,2,2};
 
     cout << Solution().canCompleteCircuit(gas, cost) << endl;
 }
