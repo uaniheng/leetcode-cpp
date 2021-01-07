4ffd5d425a55fc6dec78dd0e1db45acc5dce18ab
uaniheng <guanyicheng@boe.com.cn>
2021-01-07 10:59:04 +0800

    844 退格字符串 简单方法

--- 202010/19/_844_BackspaceStringCompare.cpp
+++ 202010/19/_844_BackspaceStringCompare.cpp
@@ -4,30 +4,33 @@
 #include "../../common.h"
 
 class Solution {
+private:
+    string disposeStr(string &s) {
+        string s2{};
+        for(char c: s) {
+            if (c == '#') {
+                if (s2.length() > 0) {
+                    s2.erase(s2.end() - 1);
+                }
+            }else {
+                s2 +=c;
+            }
+        }
+        cout << s2 << endl;
+        return s2;
+    }
+
 public:
     bool backspaceCompare(string S, string T) {
 
-        string s1, s2;
-
-        for(char c: S) {
-            if (c == '#') {
-
-            }else {
-                s1.append(c + "");
-            }
-        }
-        for(char c: T) {
-            if (c == '#') {
-                s2.pop_back();
-            }else {
-                s2.append(c +"");
-            }
-        }
-        return s1 == s2;
+        return disposeStr(S) == disposeStr(T);
     }
 };
 
 int main () {
 
-    cout << Solution().backspaceCompare("a##c", "#a#c");
+    cout << Solution().backspaceCompare("ab#c", "ad#c");
+
+    string s{};
+    s.append("")
 }
