3966aaf1851945d7cc24b8c27a3fce87da758b49
uaniheng <uaniheng@qq.com>
2021-05-11 09:31:45 +0800

    三个数的最大乘积 排序

---
+++ 202105/1/_628_MaxProductThreeNum.cpp
@@ -0,0 +1,13 @@
+//
+// Created by GYC on 2021/5/11.
+//
+#include "../../common.h"
+
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        return max(nums[n - 1] * nums[n - 2] * nums[n - 3], nums[0] * nums[1] * nums[n - 1]);
+    }
+};
