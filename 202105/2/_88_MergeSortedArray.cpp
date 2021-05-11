0fbdf776714bcdb0332305aabda9650e65bcfeaf
uaniheng <uaniheng@qq.com>
2021-05-11 15:07:09 +0800

    合并两有序数组 优化

--- 202105/2/_88_MergeSortedArray.cpp
+++ 202105/2/_88_MergeSortedArray.cpp
@@ -7,21 +7,17 @@
 class Solution {
 public:
     void merge(vector<int> &nums1, int m, vector<int> &nums2, int n) {
-        int i = 0, j = 0, curr = 0;
-        int res[m + n];
-        while (i < m || j < n) {
-            if (i == m) {
-                res[curr++] = nums2[j++];
-            } else if (j == n) {
-                res[curr++] = nums1[i++];
-            } else if (nums1[i] < nums2[j]) {
-                res[curr++] = nums1[i++];
+        int i = m - 1, j = n - 1, curr = m + n - 1;
+        while (i > -1 || j > -1) {
+            if (i == -1) {
+                nums1[curr--] = nums2[j--];
+            } else if (j == -1) {
+                nums1[curr--] = nums1[i--];
+            } else if (nums1[i] > nums2[j]) {
+                nums1[curr--] = nums1[i--];
             } else {
-                res[curr++] = nums2[j++];
+                nums1[curr--] = nums2[j--];
             }
-        }
-        for (int a = 0; a < nums1.size(); ++a) {
-            nums1[a] = res[a];
         }
     }
 };
